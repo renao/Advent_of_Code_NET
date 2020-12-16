bf3afe8390222d1f691f22f985181c2ef8b131a8
René Wernicke <r.wernicke@prosoz.de>
2020-12-16 23:55:22 +0100

    Solve day6 puzzle 2

--- Advent_of_Code/Day6.cs
+++ Advent_of_Code/Day6.cs
@@ -8,10 +8,27 @@ namespace Advent_of_Code
     {
 
         public static int CountAllQuestionsAnsweredYes(string[] groupAnswers)
-        {
-            
-            return groupAnswers
+            => groupAnswers
                 .Sum(x => x.Distinct().Count(x => char.IsLetter(x)));
+
+        public static int CountAllQuestionsEveryoneAnsweredYes(string[] groupAnswers)
+        {
+            var count = 0;
+            foreach (var group in groupAnswers)
+            {
+
+                var personalAnswers = group.Split("\n");
+                var everyonesYes = personalAnswers.First().ToCharArray().ToList();
+
+                foreach (var answer in personalAnswers)
+                {
+                    everyonesYes = everyonesYes.Intersect(answer.ToCharArray()).ToList();
+                }
+
+                count += everyonesYes.Count;
+            }
+
+            return count;
         }
     }
 }
--- Advent_of_Code_Tasks/Day6Task.cs
+++ Advent_of_Code_Tasks/Day6Task.cs
@@ -16,6 +16,12 @@ namespace Advent_of_Code_Tasks
             Assert.That(Day6.CountAllQuestionsAnsweredYes(ReadAnswersByGroup), Is.EqualTo(6259));
         }
 
+        [Test]
+        public void Count_All_Questions_which_everyone_answered_yes()
+        {
+            Assert.That(Day6.CountAllQuestionsEveryoneAnsweredYes(ReadAnswersByGroup), Is.EqualTo(3178));
+        }
+
         private string[] ReadAnswersByGroup
             => File
             .ReadAllText("day6_input")
