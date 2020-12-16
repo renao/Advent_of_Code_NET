917272928e45d4f5501344804deb1092208d5bad
René Wernicke <r.wernicke@prosoz.de>
2020-12-16 23:34:53 +0100

    Solve day6 puzzle 1

---
+++ Advent_of_Code/Day6.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent_of_Code
+{
+    public class Day6
+    {
+
+        public static int CountAllQuestionsAnsweredYes(string[] groupAnswers)
+        {
+            
+            return groupAnswers
+                .Sum(x => x.Distinct().Count(x => char.IsLetter(x)));
+        }
+    }
+}
---
+++ Advent_of_Code_Tasks/Day6Task.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using Advent_of_Code;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System;
+
+namespace Advent_of_Code_Tasks
+{
+    public class Day6_Task
+    {
+
+        [Test]
+        public void Count_All_Questions_which_were_answered_yes()
+        {
+            Assert.That(Day6.CountAllQuestionsAnsweredYes(ReadAnswersByGroup), Is.EqualTo(6259));
+        }
+
+        private string[] ReadAnswersByGroup
+            => File
+            .ReadAllText("day6_input")
+            .Split("\n\n");
+    }
+}
